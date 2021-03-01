bf3a53d21452c3e8de2c6d979cd5255bc93f4521
guanna10 <78361841+guanna10@users.noreply.github.com>
2021-03-01 09:19:49 +0100

    Add files via upload

---
+++ házi feladat vízállások.R
@@ -0,0 +1,9 @@
+## Házi feladat a vízállásokról
+mo = scan(file = "sze1960.txt")
+mo.norm = rnorm(length(mo), mean(mo), sd(mo))
+plot( mo, type = "l", xlab = "t(nap)", ylab = "h(cm)", lwd = 2)
+lines(mo.norm)
+summary(mo)
+sd(mo)*2
+IQR(mo)
+summary(mo.norm)
